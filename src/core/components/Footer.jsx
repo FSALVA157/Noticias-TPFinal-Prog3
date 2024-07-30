e29a8ea75c28d0f50fa48fbe751d47fac8221f21
Anita Gomez <120065638+AnitaGomez2183@users.noreply.github.com>
2024-07-29 21:20:03 -0400

    cambio de colores en about y agregado de text, cambio de color en el boton de sigin y agregado en home

--- src/core/components/Footer.jsx
+++ src/core/components/Footer.jsx
@@ -6,7 +6,7 @@ export const Footer = () => {
     <footer className="footer">
   <div className="content has-text-centered">
     <p>
-      <strong>Coffee Break News </strong> by <a href="https://jgthms.com">Fernando S.  Anita G.</a>.
+      <strong>Coffee Break News </strong> by <a href="https://jgthms.com">Fernando S. y Anita G.</a>.
       The source code is licensed
       <a href="https://opensource.org/license/mit">MIT</a>. The
       website content is licensed
