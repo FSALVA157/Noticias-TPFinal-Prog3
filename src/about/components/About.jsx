a8a33905d6ce86800583519b35d2610a87f29e26
Anita Gomez <120065638+AnitaGomez2183@users.noreply.github.com>
2024-07-30 21:09:41 -0400

    Agregada estructura en componenten about, agregada imagen y texto

--- src/about/components/About.jsx
+++ src/about/components/About.jsx
@@ -1,16 +1,17 @@
 import React from 'react'
+import  '../../index.css'
 
 export const About = () => {
   return (
     <>
-    <section className="hero is-danger is-fullheight">
-  <div className="hero-body">
-    <div className="">
-      <p className="title">Fullheight hero</p>
-      <p className="subtitle">Fullheight subtitle</p>
-    </div>
+  <div className="columns is-desktop">
+  <div className="column">1</div>
+  <div className="column is-8" id="about-image">
+  <img style={{width:'100%'}} src="https://cdn.pixabay.com/photo/2016/12/15/20/21/texture-1909992_1280.jpg" />
+
   </div>
-</section>
+
+</div>
     
     </>
   )
